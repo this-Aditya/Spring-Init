0a2d4803dd187914c417ba5f5da8485c51d106f1
this-Aditya <adityamishra2217@gmail.com>
2024-01-29 15:03:52 +0530

    Spring Framework initialzr added

---
+++ 01.Spring Framework/SpringFramework/src/test/java/com/springseason/SpringFramework/SpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springseason.SpringFramework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
