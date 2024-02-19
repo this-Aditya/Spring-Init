3486eec1ac4136aca8cf7a6f54cadba4dfbefaa4
this-Aditya <adityamishra2217@gmail.com>
2024-02-19 21:53:18 +0530

    Minor adjustments in jpa and jdbc's

--- 11.TODO-WEB/ToDo/src/main/java/com/todo/spring/security/SpringSecurityConfiguration.java
+++ 11.TODO-WEB/ToDo/src/main/java/com/todo/spring/security/SpringSecurityConfiguration.java
@@ -2,10 +2,13 @@ package com.todo.spring.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -31,5 +34,23 @@ public class SpringSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    /*
+        By default Spring Security provides:
+        Protection of all urls
+        A login form is shown for unauthorized requests
+        CSRF disable
+        Use of Frames is also disabled in html
+     */
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.formLogin(Customizer.withDefaults());
+        http.csrf(csrf -> csrf.disable());
+        http.headers(header -> header.frameOptions(frameOptions -> frameOptions.disable()));
+
+        return http.build();
     }
 }
